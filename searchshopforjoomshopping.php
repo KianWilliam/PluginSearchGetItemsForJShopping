5f9d63bf018b9e0a594c8b25021263539706e856
Kian William Nowrouzian <mezmer121@gmail.com>
2016-04-26 13:04:36 +0430

    Update and rename joomshopping.php to searchshopforjoomshopping.php

--- searchshopforjoomshopping.php
+++ searchshopforjoomshopping.php
@@ -1,23 +1,23 @@
 <?php 
 
 /**
- * @package Plugin plg_search_joomshopping for Joomla! 3.x
- * @version $Id: plg_search_joomshopping 1.0.0 2016-04-17 23:26:33Z $
+ * @package Plugin plg_search_searchshopforjoomshopping for Joomla! 3.x
+ * @version $Id: plg_search_searchshopforjoomshopping 1.0.0 2016-04-17 23:26:33Z $
  * @author Kian William Nowrouzian
  * @copyright (C) 2015- Kian William Nowrouzian
  * @license GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  
- This file is part of plg_search_joomshopping.
-    searchjoomshopping is free software: you can redistribute it and/or modify
+ This file is part of plg_search_searchshopforjoomshopping.
+    searchshopforjoomshopping is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
-    pluginjoomshopping is distributed in the hope that it will be useful,
+    plugin searchshopforjoomshopping is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
     You should have received a copy of the GNU General Public License
-    along with searchjoomshopping.  If not, see <http://www.gnu.org/licenses/>.
+    along with searchshopforjoomshopping.  If not, see <http://www.gnu.org/licenses/>.
  
 **/
 
@@ -25,7 +25,7 @@
 ?>
 <?php
 defined('_JEXEC') or die;
-class PlgSearchJoomshopping extends JPlugin
+class PlgSearchshopforjoomshopping extends JPlugin
 {
 	
 
