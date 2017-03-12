bf05ad0feb2411b284c9afefaddd6e1cb8b1268a
Kian William Nowrouzian <mezmer121@gmail.com>
2017-03-12 12:13:45 +0330

    Update and rename searchshopforjoomshopping.php to getitemsforjshopping.php

--- getitemsforjshopping.php
+++ getitemsforjshopping.php
@@ -1,23 +1,23 @@
 <?php 
 
 /**
- * @package Plugin plg_search_searchshopforjoomshopping for Joomla! 3.x
- * @version $Id: plg_search_searchshopforjoomshopping 1.0.0 2016-04-17 23:26:33Z $
+ * @package Plugin plg_search_getitemsforjshopping for Joomla! 3.x
+ * @version $Id: plg_search_getitemsforjshopping 1.0.0 2016-04-17 23:26:33Z $
  * @author Kian William Nowrouzian
  * @copyright (C) 2015- Kian William Nowrouzian
  * @license GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  
- This file is part of plg_search_searchshopforjoomshopping.
-    searchshopforjoomshopping is free software: you can redistribute it and/or modify
+ This file is part of plg_search_getitemsforjshopping.
+    plg_search_getitemsforjshopping is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
-    plugin searchshopforjoomshopping is distributed in the hope that it will be useful,
+    plugin plg_search_getitemsforjshopping is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
     You should have received a copy of the GNU General Public License
-    along with searchshopforjoomshopping.  If not, see <http://www.gnu.org/licenses/>.
+    along with plg_search_getitemsforjshopping.  If not, see <http://www.gnu.org/licenses/>.
  
 **/
 
@@ -25,7 +25,7 @@
 ?>
 <?php
 defined('_JEXEC') or die;
-class PlgSearchshopforjoomshopping extends JPlugin
+class PlgSearchGetitemsforjshopping extends JPlugin
 {
 	
 
@@ -85,13 +85,13 @@ class PlgSearchshopforjoomshopping extends JPlugin
 		if(!isset($cols['name_en']))
 		{
 			$db->getQuery(true);
-			$db->setQuery('ALTER #__jshopping_products ADD name_en TEXT NULL');
+			$db->setQuery('ALTER TABLE #__jshopping_products ADD name_en TEXT NULL');
 			$db->query();
 		}
 		if(!isset($cols['description_en']))
 		{
 			$db->getQuery(true);
-			$db->setQuery('ALTER #__jshopping_products ADD description_en TEXT NULL');
+			$db->setQuery('ALTER TABLE #__jshopping_products ADD description_en TEXT NULL');
 			$db->query();
 		}
 		if(!isset($cols['GB']))
@@ -105,7 +105,7 @@ class PlgSearchshopforjoomshopping extends JPlugin
         $cols = $db->getTableColumns('#__jshopping_categories');		
 		if(!isset($cols['name_en']))
 		{
-			$db->setQuery('ALTER #__jshopping_categories ADD name_en TEXT NULL');
+			$db->setQuery('ALTER TABLE #__jshopping_categories ADD name_en TEXT NULL');
 			$db->query();
 		}
 
